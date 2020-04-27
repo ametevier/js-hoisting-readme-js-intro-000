a6a96c999903334891d9b95439afc059ac4a626b
Alcee Metevier <almetevier@gmail.com>
2020-04-27 16:32:13 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,18 @@
 function callMe() {
+  var lyric = "maybe";
+  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
 }
+callMe();
 
 
 function crazy() {
-  // fix the code in here:
+  var thisIsCrazy = function () {
+    console.log("hey!!!")// fix the code in here:
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
   }
 }
 
